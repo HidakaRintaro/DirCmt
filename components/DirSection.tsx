f8ec103e3ab52fb6d617f4afec6ae63bd117a232
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-01 18:32:58 +0900

    dirSectionのコンポーネントを作成

---
+++ components/DirSection.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+interface DirSectionProps {}
+
+export const DirSection: NextPage<DirSectionProps> = (props) => {
+  return (
+    <div className="drop-shadow-lg h-96 bg-white rounded-lg w-[1024px] mx-auto">
+      <div></div>
+    </div>
+  );
+};
