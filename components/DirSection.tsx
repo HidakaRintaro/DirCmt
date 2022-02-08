863f738fb494db332ca6e8a2649fdb83b2eec146
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-08 23:21:55 +0900

    bottonを配置した

--- components/DirSection.tsx
+++ components/DirSection.tsx
@@ -1,11 +1,23 @@
-import { NextPage } from "next";
+import { FileAddIcon } from "../icons/FileAddIcon";
+import { FolderAddIcon } from "../icons/FolderAddIcon";
+import { Button } from "./IconButton";
 
 interface DirSectionProps {}
 
-export const DirSection: NextPage<DirSectionProps> = (props) => {
+export const DirSection: React.FC<DirSectionProps> = (props) => {
   return (
-    <div className="drop-shadow-lg h-96 bg-white rounded-lg w-[1024px] mx-auto">
-      <div></div>
+    <div className="drop-shadow-lg  py-2 px-4 h-96 divide-y-2 divide-gray-300 bg-white rounded-lg  mx-auto">
+      <div className="flex gap-2 mb-2">
+        <Button
+          icon={<FolderAddIcon className="stroke-white stroke-2" />}
+          value="Folder"
+        />
+        <Button
+          icon={<FileAddIcon className="stroke-white stroke-2" />}
+          value="File"
+        />
+      </div>
+      <div>folder</div>
     </div>
   );
 };
