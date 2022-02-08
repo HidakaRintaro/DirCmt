4862f0e75024af40bbc5fb1cb98acabb6fbf4bbe
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-09 01:12:10 +0900

    add downloadIcon

---
+++ icons/DownloadIcon.tsx
@@ -0,0 +1,16 @@
+import { SVGProps } from 'react'
+
+export const DownloadIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+    </svg>
+  )
+}
