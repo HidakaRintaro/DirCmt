4b6a5d0306924e7c05b406078f6ce58d10567b80
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-12 00:37:48 +0900

    add chevron icon

---
+++ icons/ChevronIcon.tsx
@@ -0,0 +1,16 @@
+import { SVGProps } from 'react'
+
+export const ChevronIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+    </svg>
+  )
+}
