a601e8375847bf4c75618f718132c0f797872163
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-12 00:48:20 +0900

    add file icon

---
+++ icons/FileIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from 'react'
+
+export const FileIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"
+      />
+    </svg>
+  )
+}
