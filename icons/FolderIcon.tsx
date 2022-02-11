b1cb48f29e2851e22a30724115d5a354cd2a09c1
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-12 00:48:34 +0900

    add folder icon

---
+++ icons/FolderIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from 'react'
+
+export const FolderIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+      />
+    </svg>
+  )
+}
