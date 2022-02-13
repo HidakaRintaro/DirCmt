89e193182a137b95968896d80f0490fb36b4c50e
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-13 19:25:53 +0900

    fiex size

--- icons/ChevronIcon.tsx
+++ icons/ChevronIcon.tsx
@@ -3,9 +3,9 @@ import { SVGProps } from 'react'
 export const ChevronIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
