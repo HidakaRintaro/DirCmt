9842e27935aa1e49b84e5225bc47c7c17d0c6c4d
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-13 19:26:15 +0900

    クリックアクションとhoverアクションを追加

--- components/DirTable.tsx
+++ components/DirTable.tsx
@@ -1,19 +1,29 @@
 import { ChevronIcon } from 'icons/ChevronIcon'
 import { FileIcon } from 'icons/FileIcon'
+import { useState } from 'react'
 
 export const DirTable: React.FC = () => {
+  // TODO: 仮データ、後データ
   let ary = []
   for (let i = 1; i < 100; i++) {
     ary.push({ id: i, name: '' })
   }
+  const [check, setCheck] = useState(false)
   return (
     <table className="w-full">
       {ary.map((a) => (
         <tr key={a.id} className="even:bg-orange-100">
           <td className="h-8 rounded-l-md pl-1">
             <div className="flex items-center gap-[2px]">
-              <span>
-                <ChevronIcon className="h-3 w-3 stroke-black stroke-2" />
+              <span
+                className="cursor-pointer rounded-sm p-1 hover:bg-gray-300"
+                onClick={() => setCheck(!check)}
+              >
+                <ChevronIcon
+                  className={`h-3 w-3 stroke-black stroke-2 text-center ${
+                    check ? 'origin-center rotate-90' : ''
+                  }`}
+                />
               </span>
               <span>
                 <FileIcon className="h-5 w-5 stroke-black stroke-2" />
