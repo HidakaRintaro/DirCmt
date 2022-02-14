846c3694fde64bc9a1c540d4336a831f2c62a65a
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-14 16:23:12 +0900

    borderと仮の固定での幅を設定

--- components/DirTable.tsx
+++ components/DirTable.tsx
@@ -13,7 +13,7 @@ export const DirTable: React.FC = () => {
     <table className="w-full">
       {ary.map((a) => (
         <tr key={a.id} className="even:bg-orange-100">
-          <td className="h-8 rounded-l-md pl-1">
+          <td className="h-8 w-40 rounded-l-md border-r-2 border-gray-200 pl-1">
             <div className="flex items-center gap-[2px]">
               <span
                 className="cursor-pointer rounded-sm p-1 hover:bg-gray-300"
@@ -31,7 +31,7 @@ export const DirTable: React.FC = () => {
               {a.id}
             </div>
           </td>
-          <td className="rounded-r-md">{a.id}</td>
+          <td className="rounded-r-md pl-2">{a.id}</td>
         </tr>
       ))}
     </table>
