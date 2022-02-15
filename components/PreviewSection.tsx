8ec62e79e3e7e6d2617486b5e948d6071b12e49b
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-15 17:00:01 +0900

    スクロール設定をした

--- components/PreviewSection.tsx
+++ components/PreviewSection.tsx
@@ -5,7 +5,7 @@ export const PreviewSection: React.FC = () => {
   const samplePreview =
     '.\n├── app      ----> application core codes\n│   ├── app      ----> collection of dependency injected\n│   ├── app      ----> collection of dependency injected\n│   ├── config   ----> config\n│   ├── domain   ----> domain layer, core business logics\n│   ├── handler  ----> (interface layer & application layer), request handlers\n│   └── dao      ----> (infrastructure layer), implementation of domain/repository\n│\n└── ddl      ----> DB definition master'
   return (
-    <div className="mx-auto mb-8 h-96 divide-y-2 divide-gray-300 rounded-lg bg-gray-800 py-2 px-4 drop-shadow-lg">
+    <div className="mx-auto mb-8 flex h-96 flex-col divide-y-2 divide-gray-300 rounded-lg bg-gray-800 py-2 px-4 drop-shadow-lg">
       <div className="mb-2 flex">
         <Button
           icon={<DownloadIcon className="stroke-white stroke-2" />}
@@ -13,9 +13,9 @@ export const PreviewSection: React.FC = () => {
           size="auto"
         />
       </div>
-      <div className="whitespace-pre-line px-2 font-mono text-gray-100">
+      <pre className="flex-1 overflow-scroll px-2 font-mono text-gray-100">
         {samplePreview}
-      </div>
+      </pre>
     </div>
   )
 }
