643242907331e977adf9bbecae8c3330bde10ee6
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-02-21 19:39:01 +0900

    ボタンコンポーネントにonClickを渡せるように修正

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,15 +4,17 @@ interface ButtonProps {
   icon?: ReactNode
   value: string
   size?: 'md' | 'lg' | 'auto'
+  onClick?: () => void
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { icon, value, size = 'md' } = props
+  const { icon, value, size = 'md', ...otherProps } = props
   return (
     <button
       className={`flex items-center justify-center gap-1 rounded-lg bg-orange-400 p-2 hover:opacity-80 ${
         size === 'md' ? 'w-28' : size === 'lg' ? 'w-36' : 'w-auto'
       }`}
+      {...otherProps}
     >
       {icon && <div>{icon}</div>}
       <div className="font-bold text-white">{value}</div>
