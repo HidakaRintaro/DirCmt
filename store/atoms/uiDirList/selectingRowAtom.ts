843bdcf0da26a4fa2d3aab9803bedbb528054cfe
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-02 00:28:24 +0900

    コメントを記述

--- store/atoms/uiDirList/selectingRowAtom.ts
+++ store/atoms/uiDirList/selectingRowAtom.ts
@@ -1,5 +1,6 @@
 import { atom } from 'recoil'
 
+// 新しいFolderやFileを追加する時用のパスの位置(FolderとFileとで値の保持方法が違う)
 export const selectingRowState = atom<string>({
   key: 'uiDirList/selectingRowState',
   default: './',
