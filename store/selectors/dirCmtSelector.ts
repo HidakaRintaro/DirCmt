05b3c77003e69267a8a74d90fcb10419631a186f
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-01 23:30:52 +0900

    セットする時にソートがかかるようにする

--- store/selectors/dirCmtSelector.ts
+++ store/selectors/dirCmtSelector.ts
@@ -1,11 +1,12 @@
 import { selector } from 'recoil'
 import { dirCmtState } from 'store/atoms/dirCmtAtom'
 import { DirCmt } from 'types/dirCmt'
+import { dirCmtSort } from 'utils/dirCmtSort'
 
 export const dirCmtSelector = selector<DirCmt[]>({
   key: 'dirCmtSelector',
   get: ({ get }) => get(dirCmtState),
   set: ({ get, set }, newValue) => {
-    set(dirCmtState, newValue)
+    set(dirCmtState, dirCmtSort(newValue as DirCmt[]))
   },
 })
