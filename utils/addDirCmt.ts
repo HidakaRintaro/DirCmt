51277162367a903967f9effd4977d0a3a9f4a13c
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-03 04:17:11 +0900

    確定して新規挿入するメソッド

--- utils/addDirCmt.ts
+++ utils/addDirCmt.ts
@@ -3,26 +3,26 @@ import { DirCmt } from 'types/dirCmt'
 export const addDirCmt = (
   selectingRow: string,
   dirCmtList: DirCmt[],
-  type: 'file' | 'directory',
+  dirCmt: DirCmt,
 ) => {
   const pathAry = selectingRow.split('/')
   pathAry.shift() // 先頭パスの . を削除
   pathAry.pop() // 末尾の空文字を削除
-  insertDirCmt(dirCmtList, pathAry, type)
+  insertDirCmt(dirCmtList, dirCmt, pathAry)
 }
 
 const insertDirCmt = (
   dirCmtList: DirCmt[],
+  addDirCmt: DirCmt,
   pathAry: string[],
-  type: 'file' | 'directory',
 ) => {
   if (pathAry.length === 0) {
-    dirCmtList.unshift({ name: '', type: type })
+    dirCmtList.push(addDirCmt)
   }
   const path = pathAry.shift()
   const dirCmt = dirCmtList.find((dirCmt) => dirCmt.name === path)
   if (dirCmt) {
     dirCmt.children = dirCmt.children ?? []
-    insertDirCmt(dirCmt.children, pathAry, type)
+    insertDirCmt(dirCmt.children, addDirCmt, pathAry)
   }
 }
