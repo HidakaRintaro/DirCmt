d1eab2010c4025b4f4f212274bd16c14306f14d7
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-03 15:00:38 +0900

    Deleteボタンを設置

--- components/DirSection.tsx
+++ components/DirSection.tsx
@@ -5,6 +5,7 @@ import { DirList } from 'components/DirList'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { selectingRowState } from 'store/atoms/uiDirList/selectingRowAtom'
 import { newRowState } from 'store/atoms/uiDirList/newRowAtom'
+import { TrashIcon } from 'icons/TrashIcon'
 
 interface DirSectionProps {}
 
@@ -18,22 +19,33 @@ export const DirSection: React.FC<DirSectionProps> = (props) => {
   const handleClickFile = () => {
     setNewRow({ isShow: true, type: 'file', selectingRow: selectingRow })
   }
+  const handleClickDelete = () => {
+    console.log('delete')
+  }
 
   return (
     <div
       className={`mx-auto mb-8 flex h-96 flex-col divide-y-2 divide-gray-300 rounded-lg bg-white py-2  px-4 drop-shadow-lg`}
     >
-      <div className="mb-2 flex gap-3">
-        <Button
-          icon={<FolderAddIcon className="stroke-white stroke-2" />}
-          value="Folder"
-          onClick={handleClickFolder}
-        />
-        <Button
-          icon={<FileAddIcon className="stroke-white stroke-2" />}
-          value="File"
-          onClick={handleClickFile}
-        />
+      <div className="mb-2 flex items-center justify-between">
+        <div className="flex gap-3">
+          <Button
+            icon={<FolderAddIcon className="stroke-white stroke-2" />}
+            value="Folder"
+            onClick={handleClickFolder}
+          />
+          <Button
+            icon={<FileAddIcon className="stroke-white stroke-2" />}
+            value="File"
+            onClick={handleClickFile}
+          />
+        </div>
+        <button
+          className="flex h-10 w-10 items-center justify-center rounded-lg bg-red-500 hover:opacity-80"
+          onClick={handleClickDelete}
+        >
+          <TrashIcon className="stroke-white stroke-2" />
+        </button>
       </div>
       <div className="flex-1 overflow-auto">
         <DirList />
