8868cf334291d515de4c7272d62ca949e872e239
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-09 00:03:50 +0900

    写真のurlを修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
         <meta property="og:site_name" content="DirCmt" />
         <meta
           property="og:image"
-          content="https://dircmt.vercel.app/facebook.png"
+          content="https://treedesc.rintaro.dev/facebook.png"
         />
         <meta property="twitter:card" content="summary" />
         <meta property="twitter:site" content="@HidakaRintaro" />
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
         />
         <meta
           property="twitter:image:src"
-          content="https://dircmt.vercel.app/twitter.png"
+          content="https://treedesc.rintaro.dev/twitter.png"
         />
       </Head>
       <header className="mt-2 text-center">
